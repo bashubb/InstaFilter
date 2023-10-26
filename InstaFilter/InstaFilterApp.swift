9c40c80909d2d52d2cfbfac6ec52cbe974bf9b91
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2023-10-26 06:07:38 +0200

    Initial Commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by HubertMac on 25/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
