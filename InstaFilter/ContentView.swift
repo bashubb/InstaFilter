f7b7188aba4e1e0ba3e5b456f9040f296ae02122
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2023-10-27 14:52:47 +0200

    Save Image
    ContentView
    - create new function save for saving choosen photo to PhotoAlbum \
    
    Imave Saver
    - created computed propeties successHandler and errorHandler for showing status of saving image

--- InstaFilter/ContentView.swift
+++ InstaFilter/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
     
     @State private var showingImagePicker = false
     @State private var inputImage: UIImage?
+    @State private var processedImage: UIImage?
     @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
     let context = CIContext()
     
@@ -85,6 +86,19 @@ struct ContentView: View {
     }
     
     func save() {
+        guard let processedImage = processedImage else {return}
+        
+        let imageSaver = ImageSaver()
+        
+        imageSaver.successHandler = {
+            print("Success")
+        }
+        
+        imageSaver.errorHandler = {
+            print("Ooops! \($0.localizedDescription)")
+        }
+        
+        imageSaver.writeToPhotoAlbum(image: processedImage)
         
     }
     
@@ -105,6 +119,7 @@ struct ContentView: View {
         if let cgimg =  context.createCGImage(outputImage, from: outputImage.extent) {
             let uiImage = UIImage(cgImage: cgimg)
             image = Image(uiImage: uiImage)
+            processedImage = uiImage
         }
     }
     
